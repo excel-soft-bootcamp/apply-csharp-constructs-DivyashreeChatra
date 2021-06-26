6665ab002505be3311b3b0a54e0b2abcee05ec5b
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-06-26 13:07:17 +0530

    updated

---
+++ BMIProgram/BMIProgram/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BMIProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BMICalulcatorConsoleInputReader display = new BMICalulcatorConsoleInputReader();
+            
+            BMICalculator obj = new BMICalculator();
+            int result = obj.CalculateBMIResult(display.GetUserHeight(), display.GetUserWeight());
+
+            Validate obj1 = new Validate();
+            string message = obj1.BMIValidate(result);
+
+            MessageDisplay obj2 = new MessageDisplay();
+            obj2.Display(message);
+
+            Console.ReadLine();
+        }
+    }
+}
