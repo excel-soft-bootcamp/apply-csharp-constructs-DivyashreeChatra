245bf162e86160346bfcd1a59f7db66791d865a5
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-06-27 19:12:30 +0530

    updated

---
+++ TataCarProgram/TataCarProgram/MultiJetEngine.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TataCarProgram
+{
+    public class MultiJetEngine : IEngine
+    {
+        public void Start()
+        {
+            Console.WriteLine("MultiJetEngine Start");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("MultiJetEngine Stop");
+        }
+    }
+}
---
+++ TataCarProgram/TataCarProgram/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TataCarProgram
+{
+    public class TataCarProgram
+    {
+        public static void Main(string[] args)
+        {
+            TATACar Car = new TATACar(new DiCorEngine());
+            Car.Drive();
+            Car.Halt();
+            TATACar multijetengine = new TATACar(new MultiJetEngine());
+            multijetengine.Drive();
+            multijetengine.Halt();
+            TATACar revorTron = new TATACar(new RevorTron());
+            revorTron.Drive();
+            revorTron.Halt();
+            Console.ReadLine();
+        }
+    }
+}
+
