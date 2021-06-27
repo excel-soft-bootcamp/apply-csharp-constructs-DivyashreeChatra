315149e0a2177b747e9367832f5d314772cdf827
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-06-27 19:26:26 +0530

    updated

---
+++ TataCarProgram/TataCarProgram/DiCorEngine.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TataCarProgram
+{
+    public class DiCorEngine : IEngine
+    {
+
+        public void Start()
+        {
+            Console.WriteLine("DiCorEngine Start");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("DiCorEngine Stop");
+        }
+    }
+}
---
+++ TataCarProgram/TataCarProgram/TATACar.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TataCarProgram
+{
+    public class TATACar
+    {
+
+        IEngine engine;
+        public TATACar(IEngine engine)
+        {
+            this.engine = engine;
+        }
+       
+        public void Drive()
+        {
+
+            engine.Start();
+
+               
+        }
+        public void Halt()
+        {
+
+            engine.Stop();
+        }
+    }
+}
