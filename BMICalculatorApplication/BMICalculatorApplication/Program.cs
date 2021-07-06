4af9dc05bdb5ecdc68e9be2d6b9da4be97e151ed
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-07-06 15:35:36 +0530

    updated

---
+++ BMICalculatorApplication/BMICalculatorApplication/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace BMICalculatorApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int height= ConsoleDisplayLib.ConsoleDisplayLibType.GetUserHeight();
+            int weight = ConsoleDisplayLib.ConsoleDisplayLibType.GetUserWeight();
+
+            int result=BMICalculatorLib.BMICalculatorType.CalculateBMIResult(height,weight);
+            string total =ValidateLib.ValidateType.BMIValidate(result);
+           
+            Console.WriteLine(total);
+            Console.WriteLine(result);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BMICalculatorApplication/ConsoleDisplayLib/ConsoleDisplayLib/ConsoleDisplayLibType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDisplayLib
+{
+    public static class ConsoleDisplayLibType
+    {
+       
+            public static int GetUserHeight()
+            {
+                int height = int.Parse(ReadFromConsole("Enter your height in meter:"));
+                return height;
+            }
+
+            public static int GetUserWeight()
+            {
+                int weight = int.Parse(ReadFromConsole("Enter your weight in kg:"));
+                return weight;
+            }
+            private static string ReadFromConsole(string message)
+            {
+
+                Console.WriteLine(message);
+                string input = Console.ReadLine();
+                return input;
+            }
+        }
+    }
+
