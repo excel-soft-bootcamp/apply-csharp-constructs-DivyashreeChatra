6e467842c89418c0491f1aa364be5ea9ded1e3bd
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-07-08 16:55:59 +0530

    updated

--- BMICalculatorApplication/BMICalculatorApplication/Program.cs
+++ BMICalculatorApplication/BMICalculatorApplication/Program.cs
@@ -3,23 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.IO;
 
 namespace BMICalculatorApplication
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
-        {
-            int height= ConsoleDisplayLib.ConsoleDisplayLibType.GetUserHeight();
-            int weight = ConsoleDisplayLib.ConsoleDisplayLibType.GetUserWeight();
+        public static int choiceValue;
 
-            int result=BMICalculatorLib.BMICalculatorType.CalculateBMIResult(height,weight);
-            string total =ValidateLib.ValidateType.BMIValidate(result);
-           
-            Console.WriteLine(total);
-            Console.WriteLine(result);
-            Console.ReadLine();
+        public enum Options
+        {
+            Console, File
         }
+        public static void Main(string[] args)
+        {
+            ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter Your Choice");
+
+            do
+            {
+                string displayMessage = $"{(int)Options.Console}->Console Input,{(int)Options.File}->File Input";
+                Console.WriteLine(displayMessage);
+                try
+                {
+                    int choiceValue = Int32.Parse(Console.ReadLine());
+                    if (choiceValue >= 2)
+                    {
+                        ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Invalid Input");
+                    }
+                    else
+                    {
+                        break;
+                    }
+                }
+                catch (FormatException ex)
+                {
+                    ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Choice must be a Number");
+                }
+
+            } while (true);
+
+            if (choiceValue == 0)
+            {
+
+                float height = ConsoleInputReaderLib.ConsoleInputReaderType.GetUserHeight();
+                float weight = ConsoleInputReaderLib.ConsoleInputReaderType.GetUserWeight();
+
+                float bmiValue = BMICalculateLib.CalculateBMIValueType.BmiCalculation(weight, height);
+                string message = BMIValueValidateLib.ValidateBMIValueType.ValidateBMIValue(bmiValue);
+                ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay(message);
+            }
+            else
+            {
+                ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter The File Path");
+                
+            }
+        }
+        
     }
 }
---
+++ BMICalculatorApplication/ConsoleInputReaderLib/ConsoleInputReaderType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleInputReaderLib
+{
+    public class ConsoleInputReaderType
+    {
+        public static int GetUserHeight()
+        {
+            ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter your height in meter:");
+            int height = int.Parse(Console.ReadLine());
+            return height;
+        }
+
+        public static int GetUserWeight()
+        {
+            ConsoleDisplayLib.ConsoleDisplayType.ConsoleDisplay("Enter your weight in kg:");
+            int weight = int.Parse(Console.ReadLine());
+            return weight;
+        }
+    }
+}
