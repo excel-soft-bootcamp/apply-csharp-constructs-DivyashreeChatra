56f036464de88a520cdbbe94d9e33bcd11ad6c9d
DivyashreeChatra <divyashreechatra5@gmail.com>
2021-07-10 09:18:14 +0530

    updated

---
+++ HomeSecurityApplication/HomeSecurityApplication/DoorSensor.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecurityApplication
+{
+    public delegate void DoorStatusHanlder(string status);
+
+        public class DoorSensor
+        {
+            public event DoorStatusHanlder DoorStatusChanged;
+            string doormovement;
+
+        public void Open()
+        {
+             doormovement = "Door is Open";
+            this.Notify();
+
+        }
+        public void Close()
+        {
+             doormovement = "Door is Closed";
+            this.Notify();
+        }
+
+        void Notify()
+        {
+            string message = $"{this.doormovement} and Time : {System.DateTime.Now.ToString()}";
+            DoorStatusChanged.Invoke(message);
+        }
+    }
+ }
+   
+
---
+++ HomeSecurityApplication/HomeSecurityApplication/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeSecurityApplication
+{
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            SecuritySystem _observer = new SecuritySystem();
+            DoorStatusHanlder _address = new DoorStatusHanlder(_observer.Notify);
+
+            DoorSensor _subject = new DoorSensor();
+            _subject.DoorStatusChanged += _address;
+            _subject.Open();
+            _subject.Close();
+
+        }
+    }
+
+}
